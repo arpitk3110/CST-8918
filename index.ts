f5e081ee77980998bfa310edcf19876e6d927a15
Arpit Patel <pate1562@algonquinlive.com>
2026-01-28 15:34:02 -0500

    hybrid 2

--- index.ts
+++ index.ts
@@ -10,6 +10,8 @@ const mongoHost = config.require("mongoHost"); // Note that strings are the defa
 const database = config.require("database");
 const nodeEnvironment = config.require("nodeEnvironment");
 const protocol = config.require("protocol")
+const mongoUsername = config.require("mongoUsername");
+const mongoPassword = config.requireSecret("mongoPassword");
 
 const stack = pulumi.getStack();
 
@@ -51,6 +53,10 @@ const mongoContainer = new docker.Container("mongoContainer", {
             aliases: ["mongo"],
         },
     ],
+    envs: [
+        `MONGO_INITDB_ROOT_USERNAME=${mongoUsername}`,
+        pulumi.interpolate`MONGO_INITDB_ROOT_PASSWORD=${mongoPassword}`,
+    ],
 });
 
 // Create the backend container
@@ -64,8 +70,8 @@ const backendContainer = new docker.Container("backendContainer", {
         },
     ],
     envs: [
-        `DATABASE_HOST=${mongoHost}`,
-        `DATABASE_NAME=${database}`,
+        pulumi.interpolate`DATABASE_HOST=mongodb://${mongoUsername}:${mongoPassword}@${mongoHost}:${mongoPort}`,
+        `DATABASE_NAME=${database}?authSource=admin`,
         `NODE_ENV=${nodeEnvironment}`,
     ],
     networksAdvanced: [
@@ -96,3 +102,7 @@ const frontendContainer = new docker.Container("frontendContainer", {
         },
     ],
 });
+
+export const url = pulumi.interpolate`http://localhost:${frontendPort}`;
+// ...
+export { mongoPassword };
